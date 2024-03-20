9d314ff1c8cf53bbbac015e9d45a6ad6c1320b5f
RAVENVILLENA <ravenvillena018@gmail.com>
2024-03-20 09:44:51 +0800

    updated

--- MVC/app/core/Model.php
+++ MVC/app/core/Model.php
@@ -2,46 +2,72 @@
 
 class Model extends Database
 {
+
+    public function __construct()
+    {
+        if (!property_exists($this, 'table')) {
+
+            $this->table = strtolower($this::class) . 's';
+        }
+    }
+   
     public function findAll()
     {
-        $query = "select * from info"; //dynamic
+        $query = "select * from $this->table";
 
-        $result = $this->query($query);
-
-        if ($result)
-        {
+        if ($result) {
             return $result;
         }
+
         return false;
     }
-
     public function where($data, $data_not = [])
     {
         $keys = array_keys($data);
-        $keys_not = array_keys($data_not);
+        $keys_not array_keys($data_not);
 
-        $query ="select * from info where ";
+        $query = "select * from $this->table where ";
 
-        foreach ($keys as $key)
-        {
-            $query .= $key . " = :" . $key . " && ";
-        }
-        
-        foreach ($keys_not as $key)
-        {
-            $query .= $key . " != :" . $key . " && ";
+        foreach ($keys as $key) {
+            $query .= $key . " = :" . $key . "&&";
         }
 
-        $query = trim($query, " && ");
-        show($query);
+        foreach ($keys_not as $key) {
+            $query .= $key . "!= :" . $key . = "&&";
+        }
 
+        $queryv = trim($query, ' && ');
+    
         $data = array_merge($data, $data_not);
         $result = $this->query($query, $data);
 
-        if ($result)
-        {
+        if ($result) {
             return $result;
         }
         return false;
     }
+    public function insert($data)
+    {
+        $columns = implode(', ', array_keys($data));
+        $values = implode(', :', array_keys($data));
+        $query = "insert into $this->table ($colums) values (:$values)";
+        show($query);
+        $this->query($query, $data);
+
+        return false;
+    }
+    public function update($id, $data, $column = 'id')
+    {
+        $keys = array_keys($data);
+        $query = "update $this->table set";
+
+        foreach ($keys as $key) {
+            $query .=$key . " = :" . $key . ", ";
+        }
+
+    
+    }
+
 }
+
+
